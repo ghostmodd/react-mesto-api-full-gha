bda2ea8a4e5b770cf048ae8b95bff3f5b99b887b
ghostmodd <ghost1mod@gmail.com>
2023-07-13 02:05:04 +0300

    fix: trying to set frontend and backend 2

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://mesto-ghostmodd.nomoredomains.work');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 app.use(reqLogger);
