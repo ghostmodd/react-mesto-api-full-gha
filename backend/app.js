65d185ab14ccf588f17d3372ed20a588392f3ad8
ghostmodd <ghost1mod@gmail.com>
2023-07-13 02:40:00 +0300

    fix: handle CORS 3

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const corsHandling = require('./middlewares/corsHandling');
 const indexRouter = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { reqLogger, errLogger } = require('./middlewares/logger');
@@ -10,10 +10,32 @@ const app = express();
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 mongoose.connect(DB_URL);
 
+const allowedCors = [
+  'https://mesto-ghostmodd.nomoredomains.work',
+  'http://mesto-ghostmodd.nomoredomains.work',
+];
+const allowedMethods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(reqLogger);
-app.use(corsHandling);
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', allowedMethods);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
 app.use('/', indexRouter);
 app.use(errLogger);
 app.use(errors());
